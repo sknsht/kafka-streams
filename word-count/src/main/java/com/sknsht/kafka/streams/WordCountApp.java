27f6bd216d5fa53d4959f4b9d7286ef20158ad9c
sofia <sofiakinsht@gmail.com>
2019-06-07 11:13:11 +0200

    Add Word Count App

---
+++ word-count/src/main/java/com/sknsht/kafka/streams/WordCountApp.java
@@ -0,0 +1,43 @@
+package com.sknsht.kafka.streams;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.KTable;
+import org.apache.kafka.streams.kstream.Materialized;
+import org.apache.kafka.streams.kstream.Produced;
+
+import java.util.Arrays;
+import java.util.Properties;
+
+public class WordCountApp {
+    public static void main(String[] args) {
+        Properties config = new Properties();
+        config.put(StreamsConfig.APPLICATION_ID_CONFIG, "wordcount-application");
+        config.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        config.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        config.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+
+        StreamsBuilder builder = new StreamsBuilder();
+
+        KStream<String, String> input = builder.stream("word-count-input");
+
+        KTable<String, Long> wordCounts = input.mapValues(text -> text.toLowerCase()) // Map values to lowercase
+                .flatMapValues(text -> Arrays.asList(text.split("\\W+"))) // FlatMap values split by space
+                .selectKey((key, word) -> word) // Select key to apply a key (discard the old key)
+                .groupByKey() // Group by key
+                .count(Materialized.as("counts")); // Count occurrences
+
+        wordCounts.toStream().to("word-count-output", Produced.with(Serdes.String(), Serdes.Long()));
+
+        KafkaStreams streams = new KafkaStreams(builder.build(), config);
+        streams.start();
+
+        // Shutdown hook to correctly close the streams application
+        Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
+    }
+}
